ee130bb776e2c843a0cdee7179f1036084617183
SirRandoo <8146689+SirRandoo@users.noreply.github.com>
2023-05-13 22:05:47 -0500

    Fixed images not loading properly.

--- _plugins/icon.rb
+++ _plugins/icon.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 module Jekyll
   class IconTag < Liquid::Tag
+    include Jekyll::Filters::URLFilters
+
     def initialize(tag_name, content, tokens)
       super
 
@@ -18,11 +20,12 @@ module Jekyll
     end
 
     def render(context)
+      @context = context
       true_src = context[@src] rescue @src
       true_alt = context[@alt] rescue @alt
-      relative_path = Liquid::Template.parse("/assets/icons/#{true_src}").render(context)
+      abs_url = absolute_url("/assets/icons/#{true_src}")
 
-      "<img loading='lazy' class='icon' src='#{relative_path}' alt='#{true_alt}'>"
+      "<img loading='lazy' class='icon' src='#{abs_url}' alt='#{true_alt}'>"
     end
   end
 end
